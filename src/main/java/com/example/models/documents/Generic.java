1beecf7af3657de7c6860580cb54c1c223ba4d3f
Maximiliano <maximilianorodrigosoria@gmail.com>
2021-07-08 13:26:41 -0300

    Generic Document is added
    Generic Document is added

--- src/main/java/com/example/models/documents/Generic.java
+++ src/main/java/com/example/models/documents/Generic.java
@@ -1,2 +1,35 @@
-package com.example.models.documents;public class Generic {
+package com.example.models.documents;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Document(collection = "generic")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+public class Generic implements Serializable {
+
+    private static final long serialVersionUID = -5824709982771246183L;
+    @Id
+    private String id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("create_at")
+    private LocalDateTime createAt;
+
+
+    public Generic() {
+        this.createAt = LocalDateTime.now();
+    }
 }
